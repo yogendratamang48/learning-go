4028fdcc28a33b07336719f105978905edfb871c
yogendratamang48 <yogen48@hotmail.com>
2021-02-25 12:37:16 +0100

    Addition of go slice

--- dev2/greetings/greetings.go
+++ dev2/greetings/greetings.go
@@ -3,13 +3,28 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 // Hello returns a greetings for the named person.
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome !", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- dev2/hello/hello.go
+++ dev2/hello/hello.go
@@ -10,7 +10,7 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 	// Get Greetings message
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Yogendra")
 	if err != nil {
 		log.Fatal(err)
 	}
